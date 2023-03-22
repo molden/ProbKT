956bb912d3838c103805aeb0a78c534247ceeb25
Martijn Oldenhof <martijn.oldenhof@kuleuven.be>
2023-03-22 14:35:41 +0100

    Removing wandb deps RCNN retraining

--- robust_detection/train/retrain_rcnn.py
+++ robust_detection/train/retrain_rcnn.py
@@ -1,22 +1,22 @@
-from robust_detection.wandb_config import ENTITY
 from robust_detection.train import fine_tune
 from robust_detection.models import fine_tune_utils
-import wandb
 import os
 import sys
 from robust_detection.models.rcnn import RCNN, RCNN_Predictor
 from robust_detection.data_utils.rcnn_data_utils import Objects_RCNN
 from argparse import ArgumentParser
-from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.loggers import WandbLogger, CSVLogger
 from robust_detection.data_utils.problog_data_utils import MNIST_Prod, MNIST_Sum, Objects_Counter, Range_Counter
 
 if __name__=="__main__":
 
     parser = ArgumentParser()
     # figure out which model to use and other basic params
-    parser.add_argument('--sweep_id', type=str, help='wandb sweep id to use')
-    parser.add_argument('--run_name', type=str, help='wandb run_name to use', default=None)
-    parser.add_argument('--sweep_id_classifier', default=None, type=str, help='wandb sweep id to use')
+#    parser.add_argument('--sweep_id', type=str, help='wandb sweep id to use')
+#    parser.add_argument('--run_name', type=str, help='wandb run_name to use', default=None)
+    parser.add_argument('--experiment_path', type=str, help='path where pretrained model was logged')
+    parser.add_argument('--output_dir', type=str, default='.', help='path where model will be logged')
+   # parser.add_argument('--sweep_id_classifier', default=None, type=str, help='wandb sweep id to use')
    # parser.add_argument('--og_data_path', type=str, help='the data the rcnn was trained on')
     parser.add_argument('--data_path', type=str, help='the model to retrain on (on top of the original one)')
     parser.add_argument('--fold', type=int, help='the fold we want to fine tune')
@@ -25,67 +25,32 @@ if __name__=="__main__":
     parser.add_argument('--target_data_type', type=str, choices=['MNIST_Prod','MNIST_Sum','Objects_Counter', 'Range_Counter'],
                                     help='Name of Data Class to use for fine tuning')
     args = parser.parse_args()
-   
+    experiment_path = args.experiment_path
+
+    dir_list = os.listdir(experiment_path)
+    dir_list = [os.path.join(experiment_path,f) for f in dir_list]
+    dir_list.sort(key=os.path.getctime, reverse=True)
+    checkpoint_file = [f for f in dir_list if "ckpt" in f][0]
     data_path = args.data_path
-    if args.run_name is None:
-       sweep_id = args.sweep_id
-       #og_data_path = args.og_data_path
-    
-       api = wandb.Api()
-       sweep = api.sweep(f"/{ENTITY}/object_detection/"+sweep_id)
-       sweep_runs = sweep.runs
-    
-       best_runs = []
-       fold = args.fold
-
-       runs_fold = [r for r in sweep_runs if (r.config.get("fold")==fold)]
-       runs_fold_sorted = sorted(runs_fold,key = lambda run: run.summary.get("restored_val_acc"), reverse = False)
-       best_run = runs_fold_sorted[0]
-
-       run_name = f"/{ENTITY}/object_detection/{best_run.id}"
-    else:
-       run_name = args.run_name
     model_cls = RCNN
     data_cls = Objects_RCNN
     target_data_cls = getattr(sys.modules[__name__], args.target_data_type)
-    logger = WandbLogger(
-      name=f"RCNN-retrain",
-      project="object_detection",
-      log_model=False
-    )
+    logger = CSVLogger(
+            f"{args.output_dir}/logger/",
+            name=f"RCNN-retrain",
+        )
     classifier = None
     #GET (FINETUNED) CLASSIFIER IF NEEDED:
-    if args.sweep_id_classifier is not None:
-        sweep_id_classifier = args.sweep_id_classifier
-        sweep = api.sweep(f"/{ENTITY}/object_detection/"+sweep_id_classifier)
-        sweep_runs = sweep.runs
-        best_runs = []
-        fold = args.fold
-
-        runs_fold = [r for r in sweep_runs if (r.config.get("fold")==fold)]
-        runs_fold_sorted = sorted(runs_fold,key = lambda run: run.summary.get("restored_val_acc"), reverse = False)
-        best_run = runs_fold_sorted[0]
-
-        run_name_classifier = best_run.id
-#    api = wandb.Api()
-        run = api.run(f"{ENTITY}/object_detection/{run_name_classifier}")
-
-        fname = [f.name for f in run.files() if "ckpt" in f.name][0]
-        run.file(fname).download(replace = True, root = ".")
-        classifier = RCNN_Predictor.load_from_checkpoint(fname)
-        classifier = classifier.classifier
-        os.remove(fname)
-
     target_data_cls.set_extra_vars(args)
     print("Creating new labels .....")
     #fine_tune_utils.relabel_data(run_name, model_cls, data_cls, target_data_path = data_path, classif=classifier, agg_case = args.agg_case, range_case=args.range_case)
-    fine_tune_utils.relabel_data(run_name, model_cls, data_cls, target_data_cls, target_data_path=data_path,
+    fine_tune_utils.relabel_data(checkpoint_file, model_cls, data_cls, target_data_cls, target_data_path=data_path,
                                              classif=classifier)
     print("Done.")
     #fine_tune_utils.relabel_detr(run_name, model_cls, data_cls, data_path = data_path)
     #re_run_id = fine_tune.re_train_detr(run_name, model_cls, data_cls, data_path, logger = logger)
     #fine_tune.fine_tune(run_name, model_cls, data_cls, target_data_path, num_epochs_dpl = 20, logger = logger)
     #re_run_id = fine_tune.re_train(run_name, model_cls, data_cls, data_path, logger = logger, agg_case = args.agg_case, range_case=args.range_case)
-    re_run_id = fine_tune.re_train(run_name, model_cls, data_cls, data_path, target_data_cls,
+    re_run_id = fine_tune.re_train(checkpoint_file, model_cls, data_cls, data_path, target_data_cls,
                                                    logger=logger)
 
